da829d35c4a2efe13f18a2fecc20e221f85aa084
Vivek Gobe <161998198+vivekgobe@users.noreply.github.com>
2025-08-03 21:22:38 +0530

    Add files via upload

---
+++ Business Problems Netflix12.sql
@@ -0,0 +1,23 @@
+-- 12 Business Problems & Solutions
+
+1. Count the number of Movies vs TV Shows
+2. Find the most common rating for movies and TV shows
+3. List all movies released in a specific year (e.g., 2020)
+4. Find the top 5 countries with the most content on Netflix
+5. Identify the longest movie
+6. Find content added in the last 5 years
+7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
+8. Count the number of content items in each genre
+9.Find each year and the average numbers of content release in India on netflix. 
+return top 5 year with highest avg content release!
+10. List all movies that are documentaries
+11. Find all content without a director
+12. Find how many movies actor 'Salman Khan' appeared in last 10 years!
+
+
+
+
+
+
+
+
